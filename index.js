4dc637b6b57d5ca9d032573c811e80ab3664f3e2
Rohan GB <114596598+RGB16@users.noreply.github.com>
2022-10-21 10:51:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var arrow_click = document.getElementById("arrow_click");
 var counter=0;
 
 bluecar.addEventListener("animationiteration",function(){
-    var random =((Math.floor(Math.random()*5))*130)
+    var random =((Math.floor(Math.random()*2))*130)
     bluecar.style.left=random+"px";
     counter++
     changeIt(counter)
